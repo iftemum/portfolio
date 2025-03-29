c23febc9f826513cb860d214faa7449243fb9875
iftemum <iftemumraian@gmail.com>
2025-03-28 18:51:02 -0600

    feature: add walls

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import { MeshReflectorMaterial } from "@react-three/drei";
 export default function App() {
   return (
     <Canvas className="canvas">
-      <ambientLight intensity={100} />
-      <directionalLight position={[5, 5, 5]} color="white" />
-      <Floor />
+      <Scene />
     </Canvas>
   );
 }
 
-const Floor = () => {
-  const {viewport} = useThree()
+// This is the main scene component
+const Scene = () => {
+  // taking the viewport size and using it to create the walls and floor
+  const { viewport } = useThree(); 
+
+  return (
+    <>
+      <ambientLight intensity={100} />
+      <directionalLight position={[5, 5, 5]} color="white" />
+      <axesHelper args={[100]} />
+      <Floor viewport={viewport} />
+      <Wall viewport={viewport} />
+    </>
+  );
+};
+
+const Floor = ({ viewport }) => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
       <planeGeometry args={[viewport.width * 2, viewport.height * 2]} />
@@ -29,5 +42,32 @@ const Floor = () => {
         metalness={0.5}
       />
     </mesh>
+  );
+};
+
+const Wall = ({ viewport }) => {
+  const wallHeight = viewport.height * 2;
+  const wallWidth = viewport.width * 2;
+
+  return (
+    <group>
+      {/* Back wall */}
+      <mesh position={[0, wallHeight / 2  - 1 , -wallHeight + 7.8 ]}>
+        <planeGeometry args={[wallWidth, wallHeight]} />
+        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.8} />
+      </mesh>
+
+      {/* Left wall */}
+      <mesh position={[-wallWidth / 2, wallHeight / 2 - 1 , 0]} rotation={[0, Math.PI / 2, 0]}>
+        <planeGeometry args={[wallHeight, wallHeight]} />
+        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.8} />
+      </mesh>
+
+      {/* Right wall */}
+      <mesh position={[wallWidth / 2, wallHeight / 2 - 1 , 0]} rotation={[0, -Math.PI / 2, 0]}>
+        <planeGeometry args={[wallHeight, wallHeight]} />
+        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.8} />
+      </mesh>
+    </group>
   );
 };
